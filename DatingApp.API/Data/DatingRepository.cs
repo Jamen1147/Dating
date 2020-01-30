55243297c267bc079efa325383946c085a17291d
Jamen1147 <jamen1147@gmail.com>
2020-01-30 18:21:47 +1100

    AutoMap from Model to DTOs

---
+++ DatingApp.API/Data/DatingRepository.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+  public class DatingRepository : IDatingRepository
+  {
+    private readonly DataContext _ctx;
+    public DatingRepository(DataContext ctx)
+    {
+      _ctx = ctx;
+    }
+    public void Add<T>(T entity) where T : class
+    {
+      _ctx.Add(entity);
+    }
+
+    public void Delete<T>(T entity) where T : class
+    {
+      _ctx.Remove(entity);
+    }
+
+    public async Task<User> GetUser(int id)
+    {
+      return await _ctx.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+    }
+
+    public async Task<IEnumerable<User>> GetUsers()
+    {
+      return await _ctx.Users.Include(p => p.Photos).ToListAsync();
+    }
+
+    public async Task<bool> SaveAll()
+    {
+      return await _ctx.SaveChangesAsync() > 0;
+    }
+  }
+}
